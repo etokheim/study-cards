75a7037aeb872f5546ab123febec628114e2b229
Erling Tokheim <e@tokheim.no>
2020-07-06 11:17:31 +0200

    Chore: Add credits for operation not permitted fix

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 // This is to battle: Error: EPERM: operation not permitted, lstat
+// Credits to llenn (https://forums.expo.io/t/error-eperm-operation-not-permitted-lstat/19221)
 const getBlacklistRE = function getBlacklistRE() {
 	return new RegExp('(.*\\android\\.*|.*\\__fixtures__\\.*|node_modules[\\\\]react[\\\\]dist[\\\\].*|website\\node_modules\\.*|heapCapture\\bundle\.js|.*\\__tests__\\.*)$')
 }
