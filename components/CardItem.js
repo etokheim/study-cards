9bb84663226ae5dab69bd8f59281e9d151740224
Erling Tokheim <e@tokheim.no>
2020-07-08 15:52:38 +0200

    Feat: Use custom buttons to flip cards

--- components/CardItem.js
+++ components/CardItem.js
@@ -1,23 +1,32 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { Card, Button } from 'react-native-paper'
+import { Card, Button, Paragraph } from 'react-native-paper'
 import { Text, Animated, Easing } from 'react-native'
 import FlipCard from 'react-native-flip-card'
 
 function CardItem({ card }) {
+	const [flipped, setFlipped] = useState(false)
+
 	return (
 		<FlipCard
 			flipHorizontal
 			flipVertical={false}
-			clickable
+			clickable={false}
+			flip={flipped}
 		>
 			{/* Face Side */}
 			<Card style={{ height: 200, marginBottom: 16 }}>
-				<Text>{ card.question }</Text>
+				<Card.Content>
+					<Paragraph>{ card.question }</Paragraph>
+					<Button onPress={() => setFlipped(true)}>Answer</Button>
+				</Card.Content>
 			</Card>
 			{/* Back Side */}
 			<Card style={{ height: 200, marginBottom: 16 }}>
-				<Text>{ card.answer }</Text>
+				<Card.Content>
+					<Paragraph>{ card.answer }</Paragraph>
+					<Button onPress={() => setFlipped(false)}>Back</Button>
+				</Card.Content>
 			</Card>
 		</FlipCard>
 	)
